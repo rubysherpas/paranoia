acbd1297ca5669a0f70e0ebe9dbff49b1fad6b47
Josh Goebel <dreamer3@gmail.com>
2012-10-22 17:11:50 -0400

    paranoia should work well with associations, unscoped is evil

--- test/paranoia_test.rb
+++ test/paranoia_test.rb
@@ -8,7 +8,8 @@ FileUtils.mkdir_p File.dirname(DB_FILE)
 FileUtils.rm_f DB_FILE
 
 ActiveRecord::Base.establish_connection :adapter => 'sqlite3', :database => DB_FILE
-ActiveRecord::Base.connection.execute 'CREATE TABLE paranoid_models (id INTEGER NOT NULL PRIMARY KEY, deleted_at DATETIME)'
+ActiveRecord::Base.connection.execute 'CREATE TABLE parent_models (id INTEGER NOT NULL PRIMARY KEY, deleted_at DATETIME)'
+ActiveRecord::Base.connection.execute 'CREATE TABLE paranoid_models (id INTEGER NOT NULL PRIMARY KEY, parent_model_id INTEGER, deleted_at DATETIME)'
 ActiveRecord::Base.connection.execute 'CREATE TABLE featureful_models (id INTEGER NOT NULL PRIMARY KEY, deleted_at DATETIME, name VARCHAR(32))'
 ActiveRecord::Base.connection.execute 'CREATE TABLE plain_models (id INTEGER NOT NULL PRIMARY KEY, deleted_at DATETIME)'
 ActiveRecord::Base.connection.execute 'CREATE TABLE callback_models (id INTEGER NOT NULL PRIMARY KEY, deleted_at DATETIME)'
@@ -68,6 +69,21 @@ class ParanoiaTest < Test::Unit::TestCase
     assert_equal 0, model.class.count
     assert_equal 1, model.class.unscoped.count
 
+  end
+  
+  def test_scoping_behavior_for_paranoid_models
+    ParanoidModel.unscoped.delete_all
+    parent1 = ParentModel.create
+    parent2 = ParentModel.create
+    p1 = ParanoidModel.create(:parent_model => parent1)
+    p2 = ParanoidModel.create(:parent_model => parent2)
+    p1.destroy
+    p2.destroy
+    assert_equal 0, parent1.paranoid_models.count
+    assert_equal 1, parent1.paranoid_models.only_deleted.count
+    p3 = ParanoidModel.create(:parent_model => parent1)
+    assert_equal 2, parent1.paranoid_models.with_deleted.count
+    assert_equal [p1,p3], parent1.paranoid_models.with_deleted
   end
 
   def test_destroy_behavior_for_featureful_paranoid_models
@@ -146,7 +162,12 @@ end
 
 # Helper classes
 
+class ParentModel < ActiveRecord::Base
+  has_many :paranoid_models
+end
+
 class ParanoidModel < ActiveRecord::Base
+  belongs_to :parent_model
   acts_as_paranoid
 end
 
