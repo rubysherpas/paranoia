30bd31a18ac536611d8ed13fa6b6f8bded8680ff
delba <delba@server.fake>
2013-06-06 11:45:07 +0200

    use where.not

--- lib/paranoia.rb
+++ lib/paranoia.rb
@@ -7,7 +7,7 @@ module Paranoia
     def paranoid? ; true ; end
 
     def only_deleted
-      all.tap { |x| x.default_scoped = false }.where("#{self.table_name}.deleted_at is not null")
+      all.tap { |x| x.default_scoped = false }.where.not(deleted_at: nil)
     end
 
     def with_deleted
