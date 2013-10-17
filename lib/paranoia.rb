ebec99bace20585c5431d61ff1473439296f7e31
Michael Dilley <michael.dilley@gmail.com>
2013-10-17 17:04:58 +1100

    Add callbacks for 'new' AR lifecycle :restore
    Fixes #72

--- lib/paranoia.rb
+++ lib/paranoia.rb
@@ -1,6 +1,7 @@
 module Paranoia
   def self.included(klazz)
     klazz.extend Query
+    klazz.extend Callbacks
   end
 
   module Query
@@ -23,6 +24,24 @@ module Paranoia
     end
   end
 
+  module Callbacks
+    def self.extended(klazz)
+      klazz.define_callbacks :restore
+
+      klazz.define_singleton_method("before_restore") do |*args, &block|
+        set_callback(:restore, :before, *args, &block)
+      end
+
+      klazz.define_singleton_method("around_restore") do |*args, &block|
+        set_callback(:restore, :around, *args, &block)
+      end
+
+      klazz.define_singleton_method("after_restore") do |*args, &block|
+        set_callback(:restore, :after, *args, &block)
+      end
+    end
+  end
+
   def destroy
     run_callbacks(:destroy) { delete }
   end
@@ -33,7 +52,7 @@ module Paranoia
   end
 
   def restore!
-    update_column :deleted_at, nil
+    run_callbacks(:restore) { update_column :deleted_at, nil }
   end
 
   def destroyed?
