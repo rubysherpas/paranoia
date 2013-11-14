8146121a89cd6706a2447e95138fd64626108959
KARASZI István <github@spam.raszi.hu>
2013-11-14 13:50:44 +0100

    removing option

--- lib/paranoia.rb
+++ lib/paranoia.rb
@@ -119,7 +119,7 @@ class ActiveRecord::Base
     class_attribute :paranoia_column
 
     self.paranoia_column = options[:column] || :deleted_at
-    default_scope { without_deleted } unless options[:default_scope] == false
+    default_scope { without_deleted }
 
     before_restore {
       self.class.notify_observers(:before_restore, self) if self.class.respond_to?(:notify_observers)
