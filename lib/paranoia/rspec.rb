65167979002544e4ab810dc5d1a306c54144f375
Emil Sågfors <emil.sagfors@iki.fi>
2014-08-12 14:24:37 +0300

    Replace deprecated RSpec failure_message_for_should syntax

--- lib/paranoia/rspec.rb
+++ lib/paranoia/rspec.rb
@@ -4,6 +4,6 @@ require 'rspec/expectations'
 RSpec::Matchers.define :act_as_paranoid do
   match { |subject| subject.class.ancestors.include?(Paranoia) }
 
-  failure_message_for_should { "#{subject.class} should use `acts_as_paranoid`" }
-  failure_message_for_should_not { "#{subject.class} should not use `acts_as_paranoid`" }
+  failure_message { "#{subject.class} should use `acts_as_paranoid`" }
+  failure_message_when_negated { "#{subject.class} should not use `acts_as_paranoid`" }
 end
