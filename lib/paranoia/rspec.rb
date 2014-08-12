08e37436a46ea479f1136ad9db337413fc97e7aa
Emil Sågfors <emil.sagfors@iki.fi>
2014-08-12 14:31:34 +0300

    Rephrase matcher failure messages according to RSpec 3 style

--- lib/paranoia/rspec.rb
+++ lib/paranoia/rspec.rb
@@ -4,6 +4,6 @@ require 'rspec/expectations'
 RSpec::Matchers.define :act_as_paranoid do
   match { |subject| subject.class.ancestors.include?(Paranoia) }
 
-  failure_message { "#{subject.class} should use `acts_as_paranoid`" }
-  failure_message_when_negated { "#{subject.class} should not use `acts_as_paranoid`" }
+  failure_message { "expected #{subject.class} to use `acts_as_paranoid`" }
+  failure_message_when_negated { "expected #{subject.class} not to use `acts_as_paranoid`" }
 end
