rubysherpas/paranoia

commit 6881e750aba90fe205fe3a91b4ed92067227dfd5

raise error for acts_as_paranoid on models without primary keys
see https://github.com/radar/paranoia/issues/169

test/paranoia_test.rb
@@ -28,6 +28,7 @@ def connect!
   ActiveRecord::Base.connection.execute 'CREATE TABLE custom_column_models (id INTEGER NOT NULL PRIMARY KEY, destroyed_at DATETIME)'
   ActiveRecord::Base.connection.execute 'CREATE TABLE custom_sentinel_models (id INTEGER NOT NULL PRIMARY KEY, deleted_at DATETIME NOT NULL)'
   ActiveRecord::Base.connection.execute 'CREATE TABLE non_paranoid_models (id INTEGER NOT NULL PRIMARY KEY, parent_model_id INTEGER)'
+  ActiveRecord::Base.connection.execute 'CREATE TABLE idless_models (deleted_at DATETIME)'
 end
 
 class WithDifferentConnection < ActiveRecord::Base
@@ -620,6 +621,12 @@ class ParanoiaTest < test_framework
     assert_equal 3, parent.very_related_models.size
   end
 
+  def test_model_without_primary_key
+    assert_raises(RuntimeError) do
+      IdlessModel.class_eval{ acts_as_paranoid }
+    end
+  end
+
   private
   def get_featureful_model
     FeaturefulModel.new(:name => "not empty")
@@ -757,4 +764,7 @@ class AsplodeModel < ActiveRecord::Base
   before_destroy do |r|
     raise StandardError, 'ASPLODE!'
   end
+end
+
+class IdlessModel < ActiveRecord::Base
 end

lib/paranoia.rb
@@ -154,6 +154,7 @@ end
 
 class ActiveRecord::Base
   def self.acts_as_paranoid(options={})
+    raise "primary key required for "+self.name unless self.primary_key
     alias :destroy! :destroy
     alias :delete! :delete
     def really_destroy!

commit df804fc2ebaa93114315854817b9a77b0758b069

Test using acts_as_paranoid class without DB
Regression from 6881e75

We need to be able to load our class definitions without having an
available database connection.

test/paranoia_test.rb
@@ -625,6 +625,14 @@ class ParanoiaTest < test_framework
     assert_equal 3, parent.very_related_models.size
   end
 
+  def test_model_without_db_connection
+    ActiveRecord::Base.remove_connection
+
+    NoConnectionModel.class_eval{ acts_as_paranoid }
+  ensure
+    setup!
+  end
+
   private
   def get_featureful_model
     FeaturefulModel.new(:name => "not empty")
@@ -762,4 +770,7 @@ class AsplodeModel < ActiveRecord::Base
   before_destroy do |r|
     raise StandardError, 'ASPLODE!'
   end
+end
+
+class NoConnectionModel < ActiveRecord::Base
 end